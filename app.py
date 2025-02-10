be4de769a531e90d1cb0b63be61b74da025480d3
ShlokP06 <shlokparikhchoco@gmail.com>
2025-02-11 00:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
-from flask import Flask, render_template, request, jsonify
-import utils
-import pandas as pd
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-
-app = Flask(__name__)
+import pickle
+import numpy as np
+from utils import hybrid_recommendation 
+app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
+# Load models and data
+with open('svd_model.pkl', 'rb') as f:
+    svd = pickle.load(f)
 
+with open('tfidf_model.pkl', 'rb') as f:
+    tfidf = pickle.load(f)
 
-@app.route("/")
+with open('user_movie_matrix.pkl', 'rb') as f:
+    user_movie_matrix = pickle.load(f)
+
+with open('content_similarity.pkl', 'rb') as f:
+    content_similarity = pickle.load(f)
+
+with open('movies.pkl', 'rb') as f:
+    movies = pickle.load(f)
+
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/recommend", methods=["POST"])
+@app.route('/recommend', methods=['POST'])
 def recommend():
     data = request.json
-    user_id = int(data.get("userId"))
-    title = data.get("title")
-    method = data.get("method")
+    user_id = int(data['user_id'])
+    title = data['title']
+  
+    if not user_id or not title:
+        return jsonify({"error": "Missing user_id or title"}), 400
+    recommendations,predicted_ratings = hybrid_recommendation(user_id, title, svd, tfidf, user_movie_matrix, content_similarity, movies)
+    return jsonify(recommendations)
 
-    if method == "CF":
-        model = utils.load_cf_model()
-        movies = pd.read_csv("movies.csv")
-        ratings = pd.read_csv("ratings.csv")
-        recs = utils.cf_recommendations(user_id, model, movies, ratings)
-    elif method == "Content":
-        cosine_sim, movies = utils.load_content_model()
-        recs = utils.content_recommendations(title, cosine_sim, movies)
-    elif method == "Hybrid":
-        cf_model = utils.load_cf_model()
-        cosine_sim, movies = utils.load_content_model()
-        ratings = pd.read_csv("ratings.csv")
-        recs = utils.hybrid_recommendations(user_id, title, cf_model, cosine_sim, movies, ratings)
 
-    return jsonify(recs.to_dict(orient="records"))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
